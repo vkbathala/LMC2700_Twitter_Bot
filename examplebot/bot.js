d5180e316678807ed56fa67f8d1a8d2b470d0c6c
jvach798 <90473940+jvach798@users.noreply.github.com>
2021-11-04 21:55:32 -0400

    Cleaned up Bot.js
    Added comments and fixed simple errors

--- examplebot/bot.js
+++ examplebot/bot.js
@@ -5,9 +5,9 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+var mediaArtsSearch = {q: "#birds", count: 10, result_type: "recent"}; 
 
-var words = ["Chirp ", "Caw ", "Hoot ", "Cluck", "Tweet"];
+var wordsOfBirds = ["Chirp ", "Caw ", "Hoot ", "Cluck", "Tweet"]; // array of that will be added to the response
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
@@ -19,17 +19,20 @@ function retweetLatest() {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
 		// ...and then we tell Twitter we want to retweet it!
-		let tweetLocation = ' https://twitter.com/TwitterDev/statuses/' + retweetId;
-		let text = data.statuses[0].text;
-		const words = text.split();
-		let length = words.length;
-		let counter = 0;
-		let birdWords = "";
-		let translation = "Bird Language: "
-		while (counter < length && translation.length + tweetLocation.length + birdWords.length + 6 < 280) {
-			birdWords += words[Math.floor(Math.random()*words.length)];
+		let tweetLocation = ' https://twitter.com/TwitterDev/statuses/' + retweetId; // This url allows for the bot to find and create a quote tweet
+		let text = data.statuses[0].text; // text of the most recent tweet that was unquoted
+		const words = text.split(" "); // splits the text into words based off of spaces
+		let length = words.length; // length of words == number of words in the Tweet
+		let counter = 0; // counter variable
+		let birdWords = ""; // Response that will be generated
+		let translation = "Bird Language: " // prefix to the Response
+		while (counter < length && translation.length + tweetLocation.length + birdWords.length + 6 < 280) { // This loop goes until the tweet has 280 characters or all the words have
+																											 // have been replaced with bird language
+			birdWords += words[Math.floor(Math.random()*wordsOfBirds.length)]; // using the Math functions, a number from 0 to wordsOfBirds.length, then is thranslated into a number 
+																			   // that the array can handle
 		}
-		T.post('statuses/update/', {status: translation + birdWords + tweetLocation}, function (error, response) {
+		T.post('statuses/update/', {status: translation + birdWords + tweetLocation}, function (error, response) { // this creates a new post, but because of tweetLocation, 
+																												   // a Quote Tweet is created
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
 			}
@@ -50,4 +53,4 @@ function retweetLatest() {
 retweetLatest();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(retweetLatest, 1000 * 60 * 5);
