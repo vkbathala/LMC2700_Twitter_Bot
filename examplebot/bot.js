dbe859e12ce7da1c4c3324ed7317b175f7a7bd8f
jvach798 <90473940+jvach798@users.noreply.github.com>
2021-11-11 15:52:45 -0500

    Finished bot.js
    Bot.js now replies, retweets, and posts the translation correctly.

--- examplebot/bot.js
+++ examplebot/bot.js
@@ -16,54 +16,55 @@ function retweetLatest() {
 	  console.log(error, data);
 	  // If our search request to the server had no errors...
 	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		const retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		let text = data.statuses[0].text; // text of the most recent tweet that was unquoted
+		let tweet = data.statuses[6]; // obtains recent tweet
+		if (tweet.retweeted_status != null) { // checks to see if tweet is a retweet
+			tweet = tweet.retweeted_status; // if so, tweet becomes original tweet and not the retweet
+		}
+		const retweetId = tweet.id_str; // id_str of the tweet
+		let text = tweet.text; // text of the most recent tweet that was unquoted
 		const words = text.split(" "); // splits the text into words based off of spaces
 		let length = words.length; // length of words == number of words in the Tweet
 		let counter = 0; // counter variable
 		let birdWords = ""; // Response that will be generated
 		let translation = "Bird Language: " // prefix to the Response
-		while (counter < length && translation.length + birdWords.length + 6 < 280) { // This loop goes until the tweet has 280 characters or all the words have
-																											 // have been replaced with bird language
-			birdWords += wordsOfBirds[Math.floor(Math.random()*wordsOfBirds.length)]; // using the Math functions, a number from 0 to wordsOfBirds.length, then is thranslated into a number 
-																			   // that the array can handle
-			counter++;
+		while (counter < length && translation.length + birdWords.length + 6 < 280) { //while counter is less than num. of words and length of new tweet is lees than 280 characters
+			birdWords += wordsOfBirds[Math.floor(Math.random()*wordsOfBirds.length)]; // adds random word from bird words
+			counter++; // word added
 		}
-		T.post('statuses/retweet/' + retweetId, {}, function (error, response) { // this creates a new post, but because of tweetLocation, 
-																												   // a Quote Tweet is created
-			if (response) {
+		T.post('statuses/retweet/' + retweetId, {}, function (error, response) {  // rewtweets the specified tweet
+			if (response) { // if success
 				console.log('Success! Check your bot, it should have retweeted something.');
 			}
 			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
+			if (error) { // else
 				console.log('There was an error with Twitter:', error);
 			}
 		});
-		const userName = data.statuses[0].user.screen_name;
-		var respond = {
-			status: `@${userName}, I Like bids`,
-			in_reply_to_status_id: retweetId
-		};
-		T.post('statuses/update', respond, function (error, response) { // this creates a new post, but because of tweetLocation, 
-			if (response) {
+		const userName = tweet.user.screen_name; // username of the author of the specified tweet
+		console.log(userName);
+		const status1 =  `@${userName} I Like birds`; // text that the bot will resond with, the @username is required to allow the bot to reply
+		const in_reply_to_status_id1 =  tweet.id_str; // id of the specified tweet
+		console.log(in_reply_to_status_id1);
+		console.log(retweetId);
+		T.post('statuses/update', {status: status1, in_reply_to_status_id: in_reply_to_status_id1}, function (error, response) { // tweets a reply to the tweet at in_reply_to_status_id1
+			if (response) { // if success
 				console.log('Success! Check your bot, it should have replied something.');
+				console.log(response);
 			}
 			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
+			if (error) { // else
 				console.log('There was an error with Twitter:', error);
 			}
 		});
-		//T.post('statuses/update', {status: translation + birdWords}, function (error, response) { // this creates a new post, but because of tweetLocation, 
-			//if (response) {
-			//	console.log('Success! Check your bot, it should have posted something.');
-			//}
+		T.post('statuses/update', {status: translation + birdWords}, function (error, response) { // this creates a new post that contains the translated text
+			if (response) { // if success
+				console.log('Success! Check your bot, it should have posted something.');
+			}
 			// If there was an error with our Twitter call, we print it out here.
-			//if (error) {
-			//	console.log('There was an error with Twitter:', error);
-			//}
-		//});
+			if (error) { // else
+				console.log('There was an error with Twitter:', error);
+			}
+		});
 	  }
 	  // However, if our original search request had an error, we want to print it out here.
 	  else {
